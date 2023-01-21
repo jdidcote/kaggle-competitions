c201f9a2846488ae2a6e202d30e6e927db23d573
joshdidcote <joshua.didcote@outlook.com>
2023-01-21 15:35:22 +0000

    Remove existing environments

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="ml_utils",
-    version="0.0.1",
-    description="ML utility functions for notebooks",
-    packages=setuptools.find_packages(),
-    python_requires='>=3.7',
-)
